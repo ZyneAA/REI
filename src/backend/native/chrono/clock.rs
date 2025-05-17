e5e670d3a106c24d445b4c3515b190f068f0bbfc
ZyneAA <htet.zyne.aa@gmail.com>
2025-05-17 19:42:21 +0630

    minor changes

---
+++ src/backend/native/chrono/clock.rs
@@ -0,0 +1,58 @@
+use std::time::{ SystemTime, UNIX_EPOCH };
+use std::fmt::Debug;
+use std::rc::Rc;
+use std::thread;
+use std::time::Duration;
+
+use crate::crux::token::{ Token, Object };
+use crate::backend::interpreter::Interpreter;
+use crate::backend::runtime_error::RuntimeError;
+use crate::backend::rei_callable::ReiCallable;
+use crate::backend::environment::Environment;
+
+#[derive(Clone, Debug)]
+pub struct TimeNow;
+impl ReiCallable for TimeNow {
+    fn arity(&self) -> usize {
+        0
+    }
+    fn call(&self, _interpreter: &mut Interpreter, _arguments: &Vec<Object>) -> Result<Object, RuntimeError<Token>> {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .map_err(|e| RuntimeError::ErrorInNativeFn { msg: e.to_string() })?
+            .as_secs_f64();
+        Ok(Object::Number(now))
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Sleep;
+impl ReiCallable for Sleep {
+    fn arity(&self) -> usize {
+        1
+    }
+
+    fn call(&self, _interpreter: &mut Interpreter, arguments: &Vec<Object>) -> Result<Object, RuntimeError<Token>> {
+        let duration = match &arguments[0] {
+            Object::Number(ms) => *ms,
+            _ => return Err(RuntimeError::ErrorInNativeFn {
+                msg: "Expected number (milliseconds) as argument to <sleep>".to_string(),
+            }),
+        };
+
+        thread::sleep(Duration::from_millis(duration as u64));
+        Ok(Object::Null)
+    }
+}
+
+pub fn register(env: &mut Environment) -> Result<(), RuntimeError<Token>> {
+
+    let callable: Rc<dyn ReiCallable> = Rc::new(TimeNow);
+    env.define("<time_now>".to_string(), Object::Callable(callable))?;
+
+    let sleep_fn: Rc<dyn ReiCallable> = Rc::new(Sleep);
+    env.define("<sleep>".to_string(), Object::Callable(sleep_fn))?;
+
+    Ok(())
+
+}
--- src/backend/native/mod.rs
+++ src/backend/native/mod.rs
@@ -2,11 +2,11 @@ use std::cell::RefMut;
 
 use crate::backend::environment::Environment;
 
-pub mod time;
+pub mod chrono;
 
 pub fn register_all_native_fns(mut env: RefMut<Environment>) -> Result<(), Box<dyn std::error::Error>> {
 
-    time::clock::register(&mut *env)?;
+    chrono::clock::register(&mut *env)?;
     Ok(())
 
 }
